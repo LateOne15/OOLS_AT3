24ca1eaedeced13af5c75604bffa68e9614f9f78
LateOne15 <48002046+LateOne15@users.noreply.github.com>
2025-10-27 14:48:09 +0800

    Update EggsOrderTest.java
    Added extra tests

--- EggsOrder/src/test/java/com/mycompany/eggsorder/EggsOrderTest.java
+++ EggsOrder/src/test/java/com/mycompany/eggsorder/EggsOrderTest.java
@@ -40,7 +40,7 @@ public class EggsOrderTest {
      * Test of calculateTotalAmount method, of class EggsOrder.
      */
     @Test
-    public void testCalculateTotalAmount() {
+    public void testCalculate1() {
         System.out.println("calculateTotalAmount");
         int eggs = 14;
         String expResult = "You ordered 14 eggs.\n" +
@@ -50,4 +50,25 @@ public class EggsOrderTest {
         assertEquals(expResult, result);
     }
     
+    @Test
+    public void testCalculate2() {
+        System.out.println("calculateTotalAmount");
+        int eggs = 24;
+        String expResult = "You ordered 24 eggs.\n" +
+            "That is 2 dozen eggs at $7.25 per dozen and 0 additional eggs\n" +
+            "at $0.75 each for a total of $14.50.";
+        String result = EggsOrder.calculateTotalAmount(eggs);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testCalculate3() {
+        System.out.println("calculateTotalAmount");
+        int eggs = 37;
+        String expResult = "You ordered 37 eggs.\n" +
+            "That is 3 dozen eggs at $7.25 per dozen and 1 additional eggs\n" +
+            "at $0.75 each for a total of $22.50.";
+        String result = EggsOrder.calculateTotalAmount(eggs);
+        assertEquals(expResult, result);
+    }
 }

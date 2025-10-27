559d7a186809a220caa6ea0ca65f4bb6c6d2eadb
LateOne15 <48002046+LateOne15@users.noreply.github.com>
2025-10-27 12:23:52 +0800

    Init
    Initial commit

---
+++ LotteryApp/src/main/java/com/mycompany/lotteryapp/LotteryApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lotteryapp;
+
+/**
+ *
+ * @author 30128198
+ */
+public class LotteryApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
